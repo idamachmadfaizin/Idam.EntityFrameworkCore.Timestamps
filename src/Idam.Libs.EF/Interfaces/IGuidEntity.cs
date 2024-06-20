3a89ac0aaf7f1fb834d5d10483ea3b39bf500dcd
Idam <idamachmadfaizin@gmail.com>
2024-06-21 04:17:07 +0700

    feat: Remove IGuidEntity.

--- src/Idam.Libs.EF/Interfaces/IGuidEntity.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Idam.Libs.EF.Interfaces;
-
-/// <summary>
-/// An Interface representation of Id as Guid instead of int
-/// </summary>
-public interface IGuidEntity
-{
-    Guid Id { get; set; }
-}
