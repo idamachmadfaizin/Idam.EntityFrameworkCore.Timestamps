d85ecbcc3f432df0cb831d65eaf08c3effa755e4
Idam Achmad Faizin <idamachmadfaizin@gmail.com>
2024-10-15 00:35:51 +0700

    feat: Implement TimeStamps UTC in the Sample project.

---
+++ Idam.EFTimestamps.Sample/Migrations/20241013165909_CreateDtUtcsTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Idam.EFTimestamps.Sample.Migrations
+{
+    /// <inheritdoc />
+    public partial class CreateDtUtcsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DtUtcs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 191, nullable: false),
+                    Description = table.Column<string>(type: "TEXT", maxLength: 191, nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    DeletedAt = table.Column<DateTime>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DtUtcs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DtUtcs");
+        }
+    }
+}
